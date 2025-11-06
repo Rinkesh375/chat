c9494f7d202b9221de7139d81876d0e37564480b
rinkesh375 <rinkeshujjwal16@gmail.com>
2025-11-06 20:30:27 +0530

    authentication better auth setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,38 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 
 export default function Home() {
-  return <Button>Click</Button>;
+  const { data: session } = authClient.useSession();
+
+  const handleSignUp = async () => {
+    try {
+      await authClient.signUp.email({
+        email: "rinkeshujjwal16@gmail.com",
+        password: "testing1234!@",
+        name: "Rinkesh",
+      });
+    } catch (error) {}
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await authClient.signOut({
+        fetchOptions: {
+          onSuccess: () => {
+            console.log("It should be redirect to guest page");
+          },
+        },
+      });
+    } catch (error) {}
+  };
+
+  return (
+    <div className="flex flex-col">
+      <Button onClick={handleSignUp}>Sign Up</Button>
+      {session?.user?.email && (
+        <Button onClick={handleSignOut}>Sign Out</Button>
+      )}
+    </div>
+  );
 }
